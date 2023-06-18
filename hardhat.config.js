247ccc6f42fc03bf2634a689ff27800b3046df84
Canberk VARLI <canberkvarli@gmail.com>
2023-06-18 02:13:59 -0700

    connect alchemy and deploy to goerli testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-ethers");
+require("@nomicfoundation/hardhat-chai-matchers")
 
-/** @type import('hardhat/config').HardhatUserConfig */
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = "BKx8lm_d_aVfLCdKisyCEidVvbhH_DGa";
+
+// Replace this private key with your Goerli account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+const GOERLI_PRIVATE_KEY = "efe2afd3483b6718e7a87645faf39b9e628245b0fc0e29d4d8de2a13bf4d9897";
+
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY]
+    }
+  }
 };
--- scripts/interact.js
+++ scripts/interact.js
@@ -4,6 +4,76 @@ async function main() {
  console.log("Getting the Canberk token contract");
  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
  const CanberkToken = await ethers.getContractAt("CanberkToken", contractAddress);
+
+  // name()
+  console.log('Querying token name...');
+  const name = await CanberkToken.name();
+  console.log(`Token Name: ${name}\n`);
+
+  // symbol()
+  console.log('Querying token symbol...');
+  const symbol = await CanberkToken.symbol();
+  console.log(`Token Symbol: ${symbol}\n`);
+
+  // decimals()
+  console.log('Querying decimals...');
+  const decimals = await CanberkToken.decimals();
+  console.log(`Token Decimals: ${decimals}\n`);
+
+
+  // totalSupply()
+  console.log('Querying token supply...');
+  const totalSupply = await CanberkToken.totalSupply();
+  console.log(`Total Supply including all decimals: ${totalSupply}`);
+  console.log(`Total supply including all decimals comma separated: ${ethers.utils.commify(totalSupply)}`);
+  console.log(`Total Supply in CAN: ${ethers.utils.formatUnits(totalSupply, decimals)}\n`);
+
+  // balanceOf(address account)
+  console.log('Getting the balance of contract owner...');
+  const signers = await ethers.getSigners();
+  const ownerAddress = signers[0].address;
+  let ownerBalance = await CanberkToken.balanceOf(ownerAddress);
+  console.log(`Contract owner at ${ownerAddress} has a ${symbol} balance of ${ethers.utils.formatUnits(ownerBalance, decimals)}\n`);
+
+  // transfer(to, amount)
+  console.log('Initiating a transfer...');
+  const recipientAddress = signers[1].address;
+  const transferAmount = 100000;
+  console.log(`Transferring ${transferAmount} ${symbol} tokens to ${recipientAddress} from ${ownerAddress}`);
+  await CanberkToken.transfer(recipientAddress, ethers.utils.parseUnits(transferAmount.toString(), decimals));
+  console.log('Transfer completed');
+  ownerBalance = await CanberkToken.balanceOf(ownerAddress);
+  console.log(`Balance of owner (${ownerAddress}): ${ethers.utils.formatUnits(ownerBalance, decimals)} ${symbol}`);
+  let recipientBalance = await CanberkToken.balanceOf(recipientAddress);
+  console.log(`Balance of recipient (${recipientAddress}): ${ethers.utils.formatUnits(recipientBalance, decimals)} ${symbol}\n`);
+
+  // approve(address spender, uint256 amount)
+  console.log(`Setting allowance amount of spender over the caller\'s ${symbol} tokens...`);
+  const approveAmount = 10000;
+  console.log(`This example allows the contractOwner to spend up to ${approveAmount} of the recipient\'s ${symbol} token`);
+  const signerContract = CanberkToken.connect(signers[1]); // Creates a new instance of the contract connected to the recipient
+  await signerContract.approve(ownerAddress, ethers.utils.parseUnits(approveAmount.toString(), decimals));
+  console.log(`Spending approved\n`);
+
+
+  // transferFrom(address from, address to, uint256 amount)
+  const transferFromAmount = 100;
+  console.log(`contracOwner transfers ${transferFromAmount} ${symbol} from recipient\'s account into own account...`);
+  await CanberkToken.transferFrom(recipientAddress, ownerAddress, ethers.utils.parseUnits(transferFromAmount.toString(), decimals));
+  ownerBalance = await CanberkToken.balanceOf(ownerAddress);
+  console.log(`New owner balance (${ownerAddress}): ${ethers.utils.formatUnits(ownerBalance, decimals)} ${symbol}`);
+  recipientBalance = await CanberkToken.balanceOf(recipientAddress);
+  console.log(`New recipient balance (${recipientAddress}): ${ethers.utils.formatUnits(recipientBalance, decimals)} ${symbol}`);
+  allowance = await CanberkToken.allowance(recipientAddress, ownerAddress);
+  console.log(`Remaining allowance: ${ethers.utils.formatUnits(allowance, decimals)} ${symbol}\n`);
+
+
+  // increaseAllowance(address spender, uint256 addedValue)
+  const incrementAmount = 100;
+  console.log(`Incrementing contractOwner allowance by ${incrementAmount} ${symbol}...`);
+  await signerContract.increaseAllowance(ownerAddress, ethers.utils.parseUnits(incrementAmount.toString(), decimals));
+  allowance = await CanberkToken.allowance(recipientAddress, ownerAddress);
+  console.log(`Updated allowance: ${ethers.utils.formatUnits(allowance, decimals)} ${symbol}\n`);
 }
 
 main()
